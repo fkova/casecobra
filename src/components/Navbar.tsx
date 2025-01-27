88e88c8efaf8695d0bc5c2a77b5eb7f27755f3a9
DESKTOP-MSGBHCN\Rahzel <tobiasmoretty@gmail.com>
2025-01-27 20:33:54 +0100

    copy paste all, to avoid killing my time with css overdose

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,82 @@
-import Link from "next/link"
-import MaxWidthWrapper from "./MaxWidthWrapper"
-import { buttonVariants } from "./ui/button";
-import { ArrowRight } from "lucide-react";
-import { auth } from "@/auth";
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import { ArrowRight } from 'lucide-react'
+import { auth } from '@/auth'
 
 const Navbar = async () => {
-    const session = await auth();
-    const user = session?.user;
-    const isAdmin = session?.user?.email === process.env.ADMIN_EMAIL;
+  const session = await auth();
+  const user = session?.user;
+  const isAdmin = session?.user?.email === process.env.ADMIN_EMAIL;
 
-    return (<nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-        <MaxWidthWrapper>
-            <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-                <Link href='/' className="flex z-40 font-semibold">
-                    case<span className="text-green-600">cobra</span>
+  return (
+    <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+      <MaxWidthWrapper>
+        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+          <Link href='/' className='flex z-40 font-semibold'>
+            case<span className='text-green-600'>cobra</span>
+          </Link>
+
+          <div className='h-full flex items-center space-x-4'>
+            {user ? (
+              <>
+                <Link
+                  href='/api/auth/signout'
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}>
+                  Sign out
                 </Link>
-                <div className="h-full flex items-center space-x-4">
-                    {user ? (<>
-                        <Link
-                            href='/api/auth/signout'
-                            className={buttonVariants({ size: 'sm', variant: 'ghost' })}>
-                            Sign out
-                        </Link>
+                {isAdmin ? (
+                  <Link
+                    href='/dashboard'
+                    className={buttonVariants({
+                      size: 'sm',
+                      variant: 'ghost',
+                    })}>
+                    Dashboard ✨
+                  </Link>
+                ) : null}
+                <Link
+                  href='/configure/upload'
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}>
+                  Create case
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href='/api/auth/signin'
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}>
+                  Sign up / Login
+                </Link>
 
-                        {isAdmin && <Link
-                            href='/dashboard'
-                            className={buttonVariants({ size: 'sm', variant: 'ghost' })}>
-                            Dashboard ✨
-                        </Link>}
-                    </>) : (<>
-                        {/* <Link
-                            href='/api/auth/register'
-                            className={buttonVariants({ size: 'sm', variant: 'ghost' })}>
-                            Sign up
-                        </Link> */}
+                <div className='h-8 w-px bg-zinc-200 hidden sm:block' />
 
-                        <Link
-                            type="submit"
-                            href='/api/auth/signin'
-                            className={buttonVariants({ size: 'sm', className: 'hidden sm:flex bg-red-800' })}>
-                            Sign In With Google
-                        </Link>
-
-                        <div className="h-8 w-px bg-zinc-200 hidden sm:block"></div>
-                    </>)}
-                    <Link
-                        href='/configure/upload'
-                        className={buttonVariants({ size: 'sm', className: 'hidden sm:flex items-center gap-1' })}>
-                        Create case <ArrowRight className="m1-1.5 h-5 w-5" />
-                    </Link>
-                </div>
-            </div>
-        </MaxWidthWrapper>
-    </nav >)
+                <Link
+                  href='/configure/upload'
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'hidden sm:flex items-center gap-1',
+                  })}>
+                  Create case
+                  <ArrowRight className='ml-1.5 h-5 w-5' />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
 }
-
 
 export default Navbar
